d28dc7c639100ab74b82edbc80977773a5feadb3
William <william_burns1@baylor.edu>
2023-01-30 14:12:52 -0600

    dflsdkjfasd;f

--- 1_dictionary start file.py
+++ 1_dictionary start file.py
@@ -1,158 +1,65 @@
 import random
 
-phonebook = {'Chris':'555−1111',
-             'Katie':'555−2222',
-             'Joanne':'555−3333'}
+# Dictionaries— dict = {'key1':'value1', 'key2':'value2', 'key3':'value3', ...}
+# school_data.json is a list of dictionaries (values of list entries can be any python object)
+# methods are functions applied to objects (appended with a ".")
+emptyDict = {}
+phonebook = {'Chris': '555−1111',
+             'Katie': '555−2222',
+             'Joanne': '555−3333'}
 
+myDict = dict(m=8, n=9)
+print(f"Number of key-calue paires: {len(phonebook)}")
 
 
-print()
-print('*****  start section 1 - print dictionary ********')
-print()
+# Key error: no match for the key you chose (case sensitive)
+# Dictionary keys have to be unique (no repeating key values)
+# Dictionaries are mutable (you can edit the values)
+name = 'chris'
+if name in phonebook:
+    print(phonebook[name])
+else:
+    print('name not found')
 
+try:
+    print(phonebook[name])
+except KeyError:
+    print('name not found')
 
+print(phonebook['Chris'])
 
+print(phonebook)
+phonebook['Chris'] = '555-4444'
+phonebook['Joe'] = '555-0123'
+print(phonebook)
 
+print(phonebook['Chris'])
+del phonebook['Chris']
 
-print()
-print('*****  end section 1 ********')
-print()
+for key in phonebook:
+    print(key, ': ', phonebook[key])
 
+for value in phonebook.values():
+    print(value)
 
-'''
+for k, v in phonebook.items():
+    print(f'the key is {k} and the value is {v}')
 
+for ph_tuple in phonebook.items():
+    print(ph_tuples)
 
-print()
-print('*****  start section 2 - search dictionary ********')
-print()
+name = 'Katie'
+phonebook.get(name, 'key not found')
 
+print(phonebook)
+phonebook.clear()
+print(phonebook)
 
+remove = phonebook.pop('Katie', 'not found')
+print(remove)
 
+a = phonebook.popitem()
+print(a)
+print(phonebook)
 
-
-
-
-print()
-print('*****  end section 2 ********')
-print()
-
-
-
-
-
-
-
-print()
-print('*****  start section 3 - edit/append dictionary ********')
-print()
-
-
-
-
-
-print()
-print('*****  end section 3 ********')
-print()
-
-
-
-
-
-
-print()
-print('*****  start section 4 - delete/remove from dictionary ********')
-print()
-
-
-
-
-print()
-print('*****  end section 4 ********')
-print()
-
-
-
-
-
-
-print()
-print('*****  start section 5 - iterate through keys, values, items ********')
-print()
-
-
-
-
-
-
-print()
-print('*****  end section 5 ********')
-print()
-
-
-
-
-
-print()
-print('*****  start section 6 - using get and clear ********')
-print()
-
-
-
-
-
-
-print()
-print('*****  end section 6 ********')
-print()
-
-
-
-print()
-print('*****  start section 7 - using pop method ********')
-print()
-
-
-
-
-
-
-print()
-print('*****  end section 7 ********')
-print()
-
-
-
-print()
-print('*****  start section 8 - using popitem ********')
-print()
-
-
-
-
-
-
-print()
-print('*****  end section 8 ********')
-print()
-
-
-
-print()
-print('*****  start section 9 - using random and converting to list ********')
-print()
-
-
-
-
-
-print()
-print('*****  end section 9 ********')
-print()
-
-
-'''
-
-
-
-
-
+print(phonebook[random.choice(list(phonebook))])
--- 2_person_dict.py
+++ 2_person_dict.py
@@ -5,3 +5,11 @@ person["age"] = 51
 person["spouse"] = "Edna"
 person["children"] = ["Ralph", "Betty", "Joey"]
 person["pets"] = {"dog": "Fido", "cat": "Sox"}
+
+print(person)
+person['children'][1]
+person['pets']['cat']
+for index in range(len(person['children'])):
+    print(person['children'][index])
+for i, j in person['pets'].items():
+    print(f"animal: {i}, name: {j}")
